c9518d8e5d7a2de9a7eaae4ac87d03bd8bcd5437
Tej Kolpek <110253633+tejaskolpek@users.noreply.github.com>
2025-03-04 22:33:30 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -27,14 +27,14 @@ for directory in [
 HOPSWORKS_API_KEY = os.getenv("HOPSWORKS_API_KEY")
 HOPSWORKS_PROJECT_NAME = os.getenv("HOPSWORKS_PROJECT_NAME")
 
-FEATURE_GROUP_NAME = "time_series_hourly_feature_group"
+FEATURE_GROUP_NAME = "time_series_hourly_feature_group_v2"
 FEATURE_GROUP_VERSION = 1
 
-FEATURE_VIEW_NAME = "time_series_hourly_feature_view"
+FEATURE_VIEW_NAME = "time_series_hourly_feature_view_v2"
 FEATURE_VIEW_VERSION = 1
 
 
-MODEL_NAME = "taxi_demand_predictor_next_hour"
+MODEL_NAME = "taxi_demand_predictor_next_hour_v2"
 MODEL_VERSION = 1
 
-FEATURE_GROUP_MODEL_PREDICTION = "taxi_hourly_model_prediction"
+FEATURE_GROUP_MODEL_PREDICTION = "taxi_hourly_model_prediction_v2"
